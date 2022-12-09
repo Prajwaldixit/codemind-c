fc995c5aadcb57801b85dc652e804b0d4c6af757
Prajwaldixit <119730576+Prajwaldixit@users.noreply.github.com>
2022-12-09 15:35:31 +0530

    #include<stdio.h> int main() {     int i,p=1;     int a[3];     for(i=0;i<3;i++)     {         scanf("%d",&a[i]);         p=p*a[i];     }     printf("%dKB",p);
    }

---
+++ Find_the_Capacity.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,p=1;
+    int a[3];
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+        p=p*a[i];
+    }
+    printf("%dKB",p);
+    
+    
+    
+}
