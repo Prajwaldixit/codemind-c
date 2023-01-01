9b63dc875b5cbc18e4e39d62d43a6d97fff5368d
Prajwaldixit <119730576+Prajwaldixit@users.noreply.github.com>
2023-01-01 12:05:13 +0530

    #include<stdio.h> int main() {     int n,c=0,oc=0,ec=0,r;     scanf("%d",&n);     while(n>0)     {         r=n%10;         if(r%2==0)         {             ec++;         }         else if(r%2!=0)         {             oc++;         }         n=n/10;         c++;     }     if(c==ec)     {         printf("Even");     }     else if (c==oc)     {         printf("Odd");     }     else printf("Mixed");
    }

---
+++ Even_Odd_Mixed.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,oc=0,ec=0,r;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        if(r%2==0)
+        {
+            ec++;
+        }
+        else if(r%2!=0)
+        {
+            oc++;
+        }
+        n=n/10;
+        c++;
+    }
+    if(c==ec)
+    { 
+        printf("Even");
+    }
+    else if (c==oc)
+    { 
+        printf("Odd");
+    }
+    else printf("Mixed");
+    
+}
