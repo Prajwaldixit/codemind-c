6127e2f42abb2c85d598f381aca4901eb1e562ce
Prajwaldixit <119730576+Prajwaldixit@users.noreply.github.com>
2023-05-08 22:20:37 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,i,c,ct=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         c=a+b;         if(c==n)         {             ct++;             break;         }         a=b;         b=c;
        }
        if(ct==0)printf("False");
        else printf("True");
    }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,i,c,ct=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        c=a+b;
+        if(c==n)
+        {
+            ct++;
+            break;
+        }
+        a=b;
+        b=c;
+        
+    }
+    if(ct==0)printf("False");
+    else printf("True");
+}
