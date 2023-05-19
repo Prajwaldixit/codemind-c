5d860fa69ea73bc24f24006371ad8132ee76370a
Prajwaldixit <119730576+Prajwaldixit@users.noreply.github.com>
2023-05-19 20:50:38 +0530

    #include<stdio.h> int main() {     int n,q,r,s;     scanf("%d",&n);     q=n;     while(q>9)     {         s=0;         while(q!=0)         {             r=q%10;             s=s+r*r;             q=q/10;         }         if(s>9)         {             q=s;         }     }     if(s==1||s==7)     {         printf("True");     }     else printf("False"); }

--- happy_number.c
+++ happy_number.c
@@ -1,19 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,s=0;
+    int n,q,r,s;
     scanf("%d",&n);
-    while(n!=0)
+    q=n;
+    while(q>9)
     {
-        r=n%10; 
-        n=n/10;
-        s=s+r*r;
-        if(n==0&&s>9)
+        s=0;
+        while(q!=0)
         {
-            n=s;
-            s=0;
+            r=q%10;
+            s=s+r*r;
+            q=q/10;
+        }
+        if(s>9)
+        {
+            q=s;
         }
     }
-    if(s==1 || s==7) printf("True");
+    if(s==1||s==7)
+    {
+        printf("True");
+    }
     else printf("False");
 }
