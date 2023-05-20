f5b4cbc13a7385152a502bf79291def3bc8a39e7
Prajwaldixit <119730576+Prajwaldixit@users.noreply.github.com>
2023-05-20 21:15:09 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n-i;j>=1;j--)         {             printf(" ");         }         for(j=1;j<=n;j++)         {             if(i==n || j==n||i==1||j==1){                 printf("*");             }             else{                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k;
     scanf("%d",&n);
-    for(i=65;i<=65+n-1;i++)
+    for(i=1;i<=n;i++)
     {
+        for(j=n-i;j>=1;j--)
+        {
+            printf(" ");
+        }
         for(j=1;j<=n;j++)
         {
-            printf("%c ",i);
+            if(i==n || j==n||i==1||j==1){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
         }
         printf("
 ");
